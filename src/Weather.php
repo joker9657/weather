baaffd1ed140df377073857f8df9856fa7d4b33b
joker9657 <zqf2102398@gmail.com>
2020-09-12 15:09:30 +0800

    update Refactor code

--- src/Weather.php
+++ src/Weather.php
@@ -23,21 +23,21 @@ class Weather
         return new Client($this->guzzleOptions);
     }
 
-    public function setHttpClient(array $options)
+    public function setGuzzleOptions(array $options)
     {
         $this->guzzleOptions = $options;
     }
 
-    public function getWeather($city, string $type = 'base', string $format = 'json')
+    public function getWeather(string $city, string $type = 'base', string $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
-        if (!\in_array(\strtolower($format), ['xml', 'json'])) {
+        if (!in_array(strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$format);
         }
 
         if (!\in_array(\strtolower($type), ['base', 'all'])) {
-            throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
+            throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
         }
 
         $query = array_filter([
@@ -55,5 +55,31 @@ class Weather
         } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
+    }
+
+    /**
+     * 获取实时天气
+     * @param string $city
+     * @param string $format
+     * @return mixed|string
+     * @throws HttpException
+     * @throws InvalidArgumentException
+     */
+    public function getLiveWeather(string $city, string $format = 'json')
+    {
+        return $this->getWeather($city, 'base', $format);
+    }
+
+    /**
+     * 获取天气预报
+     * @param string $city
+     * @param string $format
+     * @return mixed|string
+     * @throws HttpException
+     * @throws InvalidArgumentException
+     */
+    public function getForecastsWeather(string $city, string $format = 'json')
+    {
+        return $this->getWeather($city, 'all', $format);
     }
 }
